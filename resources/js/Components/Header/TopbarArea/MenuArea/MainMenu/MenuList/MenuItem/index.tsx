ddfe74e076fc4e351724cd3697a672db76cfadb9
maximusbro <shidlovskiy.maksimka@gmail.com>
2023-05-25 14:30:43 -0600

    check

--- resources/js/Components/Header/TopbarArea/MenuArea/MainMenu/MenuList/MenuItem/index.tsx
+++ resources/js/Components/Header/TopbarArea/MenuArea/MainMenu/MenuList/MenuItem/index.tsx
@@ -27,7 +27,6 @@ const MenuItem: React.FC<MenuItemProps> = ({ title, hrefTitle, list }) => {
 					submenu.current,
 					{
 						height: showAccordion ? "auto" : 0,
-						opacity: showAccordion ? 1 : 0
 					},
 					{ duration: 0.2 },
 				)
@@ -40,7 +39,7 @@ const MenuItem: React.FC<MenuItemProps> = ({ title, hrefTitle, list }) => {
 		<li className={showAccordion ? "menu-item-has-children active" : "menu-item-has-children"}>
 			<a href={hrefTitle} className="drop-down" > {title}</a >
 			<i onClick={toggleShowCardion} className={showAccordion ? "bi bi-plus dropdown-icon active" : "bi bi-plus dropdown-icon"}></i>
-			<ul ref={submenu} className="sub-menu" style={showAccordion ? { opacity: 1 } : { opacity: 0 }}>
+			<ul ref={submenu} className="sub-menu" style={showAccordion ? { opacity: 1, } : { opacity: 0 }}>
 				{list.map((item, index) => {
 					const { listTitle, listHref } = item;
 					return (

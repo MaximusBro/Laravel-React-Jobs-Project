dc1163bc3520c9a12292d1a7775d3df2abcb2cf3
maximusbro <shidlovskiy.maksimka@gmail.com>
2023-05-25 09:33:15 -0600

    Animation for accordion in burger menu

--- resources/js/Components/Header/TopbarArea/MenuArea/MainMenu/MenuList/MenuItem/index.tsx
+++ resources/js/Components/Header/TopbarArea/MenuArea/MainMenu/MenuList/MenuItem/index.tsx
@@ -12,7 +12,7 @@ interface listProps {
 }
 
 const MenuItem: React.FC<MenuItemProps> = ({ title, hrefTitle, list }) => {
-	const [isPresent, safeToRemove] = usePresence();
+	const [isPresent] = usePresence();
 	const [submenu, animateSubmenu] = useAnimate();
 	const [showAccordion, setShowAccordion] = useState(false);
 
@@ -25,7 +25,12 @@ const MenuItem: React.FC<MenuItemProps> = ({ title, hrefTitle, list }) => {
 			const enterAnimation = async () => {
 				await animateSubmenu(
 					submenu.current,
-					{ height: [0, 200] }, { duration: 0.5 })
+					{
+						height: showAccordion ? "auto" : 0,
+						opacity: showAccordion ? 1 : 0
+					},
+					{ duration: 0.2 },
+				)
 			}
 			enterAnimation()
 		}
@@ -35,7 +40,7 @@ const MenuItem: React.FC<MenuItemProps> = ({ title, hrefTitle, list }) => {
 		<li className={showAccordion ? "menu-item-has-children active" : "menu-item-has-children"}>
 			<a href={hrefTitle} className="drop-down" > {title}</a >
 			<i onClick={toggleShowCardion} className={showAccordion ? "bi bi-plus dropdown-icon active" : "bi bi-plus dropdown-icon"}></i>
-			<ul ref={submenu} className="sub-menu" style={showAccordion ? { display: "block" } : {}}>
+			<ul ref={submenu} className="sub-menu" style={showAccordion ? { opacity: 1 } : { opacity: 0 }}>
 				{list.map((item, index) => {
 					const { listTitle, listHref } = item;
 					return (
